410afc5c402c0bceceedcda5b87bef7ce31fde88
Galder <galder999@hotmail.com>
2023-03-05 16:41:51 +0100

    Proyecto Base

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ public class Main {
             miCuenta.retirar(2300);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
+            
         }
         try {
             System.out.println("Ingreso en cuenta");
